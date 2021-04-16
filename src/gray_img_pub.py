2b18ee2ce1a015a0a251b9ecc8046aa36ef0d088
Adam Ring <adamring@brandeis.edu>
2021-04-16 16:09:49 +0000

    changes

--- src/gray_img_pub.py
+++ src/gray_img_pub.py
@@ -21,7 +21,7 @@ class color2Gray:
     def img_cb(self, img):
         
         cv_img = self.bridge.imgmsg_to_cv2(img, 'bgr8')
-        gray_cv_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray_cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
         gray_img = self.bridge.cv2_to_imgmsg(gray_cv_img)
         self.img_pub.publish(gray_img)
 
@@ -31,4 +31,4 @@ class color2Gray:
 if __name__ ==  "__main__":
     rospy.init_node('gray_img_pub')
     color_2_gray = color2Gray()
-    rospy.spin
+    rospy.spin()
