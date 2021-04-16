379282517c0d60bb001bed52ba1f6176f877a8e2
NathanCai2000 <nathancai@brandeis.edu>
2021-04-16 16:19:03 +0000

    Cleaning Commit

--- src/detect_people.py
+++ src/detect_people.py
@@ -19,7 +19,7 @@ class personDetector:
 
         self.person_pub = rospy.Publisher('/people', Int32MultiArray, queue_size=1)
 
-        self.img_pub = rospy.Publisher('/person_img')
+        self.img_pub = rospy.Publisher('/person_img', Image, queue_size=1)
 
         self.person_arr = Int32MultiArray()
 
@@ -37,23 +37,23 @@ class personDetector:
         self.color_img = self.bridge.imgmsg_to_cv2(color_img, 'bgr8')
 
     def detect_people(self):
-        try:
-            person_list = []
-            people, _ = self.hog.detectMultiScale(self.gray_img, winStride(8,8), scale=1.5)
-            for (x, y, w, h) in people:
-                mass = w*h
-                person_list.append([x, mass])
-                cv2.rectangle(self.color_img,(x,y),(x+w,y+h),(0,0,255),2)
-            color_img_msg = self.bridge.cv2_to_imgmsg(self.color_img, 'bgr8')
-            self.img_pub.publish(color_img_msg)
-            self.person_arr.data = person_list
-            self.person_pub.publish(self.person_arr)
-        except:
-            pass
+        
+        person_list = []
+        people, _ = self.hog.detectMultiScale(self.gray_img, winStride=(8,8), scale=1)
+        for (x, y, w, h) in people:
+            mass = w*h
+            person_list.append([x, mass])
+            cv2.rectangle(self.color_img,(x,y),(x+w,y+h),(0,0,255),2)
+        color_img_msg = self.bridge.cv2_to_imgmsg(self.color_img, 'bgr8')
+        self.img_pub.publish(color_img_msg)
+        self.person_arr.data = person_list
+        self.person_pub.publish(self.person_arr)
 
 if __name__ == "__main__":
     
+    rospy.init_node('person_detector')
     person_detector = personDetector()
+    rospy.sleep(1)
     while not rospy.is_shutdown():
         person_detector.detect_people()
         rospy.sleep(0.017)
