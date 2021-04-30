b4315f214bc903850afad2d3531e931534c98a7b
Adam Ring <adamring@brandeis.edu>
2021-04-30 22:47:06 +0000

    Minor changes

--- src/model.py
+++ src/model.py
@@ -2,9 +2,13 @@
 
 # Must run the following command in your workspace to install torch, torchvision, and future
 # pip install torch==1.4.0 torchvision==0.5.0
-# pip install futuregf
+# pip install future
 
 # Additionally, this model requires you to have the path "../torch_model/model_state_dict.pth"
+# The file for the state dictionary for the model exceeds github's file size limit, so go to the following
+# link to download the state dictionary. Make sure it is put in a folder named "torch_model" in the main folder
+# for this project.
+# Link: https://drive.google.com/file/d/1n1nBDpdu9GnAb006depSl32x6O47NU_D/view?usp=sharing 
 
 import torch
 import torchvision
@@ -66,15 +70,16 @@ class FaceRCNN:
         # print(os.getcwd())
 
         self.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-        print(self.device)
+        print("Model is running on {}".format(self.device))
         
         self.model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
         # get number of input features for the classifier
         in_features = self.model.roi_heads.box_predictor.cls_score.in_features
         # replace the pre-trained head with a new one
         self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 3)
-        self.model.load_state_dict(torch.load('src/Term_Project/torch_model/model_state_dict.pth'))        
-        # self.model = torch.load('src/Term_Project/torch_model/entire_model.pth')
+
+        # Load the pretrained weights of the model from the downloaded file.
+        self.model.load_state_dict(torch.load('src/Term_Project/torch_model/model_state_dict.pth'))
 
         self.model.to(self.device)
         self.model.eval()
@@ -82,7 +87,7 @@ class FaceRCNN:
         self.labels_dict =  {1:'Person', 2:'Face'}
         self.colors_dict = {1:(0,255,0), 2:(0,0,255)}
 
-
+    # Prints the number of parameters in the model to the terminal
     def get_n_params(self):
         pp=0
         for p in list(self.model.parameters()):
@@ -93,66 +98,25 @@ class FaceRCNN:
         print(pp)
 
     def simple_detection(self, img):
+        # Preprocessing the input image
         start = time()
         image_tensor = torch.from_numpy(img).type(torch.FloatTensor).to(self.device)
         image_tensor = torch.transpose(image_tensor, 0,2)
         image_tensor = torch.transpose(image_tensor, 1,2)
-        print(image_tensor.shape)
         image_tensor_normal = image_tensor/255
         duration = time() - start
         print("\nPreprocessing: {} s".format(duration))
 
-        # try:
+        # Passing preprocessed image into model
         start = time()
         output = self.model([image_tensor_normal])
         duration = time() - start
         print("\nModel processing: {} s".format(duration))
+
+        # Returns the bounding boxes, labels for each bounding box, and the prediction probabilities for each box
         boxes = output[0]['boxes']
         labels = output[0]['labels']
         scores = output[0]['scores']
-        # inds_clean_scores = []
-        # for i in range(scores.shape[0]):
-        #     if scores[i] >= 0.8:
-        #         inds_clean_scores.append(i)
-        # inds_clean_scores = torch.LongTensor(inds_clean_scores, device=self.device)
-        # clean_boxes = torch.index_select(boxes, 0, inds_clean_scores)
-        # clean_labels = torch.index_select(labels, 0, inds_clean_scores)
-        # return clean_boxes, clean_labels
         return boxes.detach().numpy(), labels.detach().numpy(), scores.detach().numpy()
-        # except:
-        #     pass
 
 
-
-
-    def person_boxes(self, img):
-        image_tensor = torch.from_numpy(img).type(torch.FloatTensor).to(self.device)
-        image_tensor = torch.transpose(image_tensor, 0,2)
-        image_tensor = torch.transpose(image_tensor, 1,2)
-        image_tensor_normal = image_tensor/255
-        output = self.model([image_tensor_normal])
-        print('processed the output')
-        boxes = output[0]['boxes'].numpy()
-        print(boxes)
-        labels = output[0]['labels'].numpy()
-        print(labels)
-        scores = output[0]['scores'].numpy()
-        print(scores)
-        inds_clean_scores = []
-        for i in range(scores.shape[0]):
-            if scores[i] >= 0.8:
-                inds_clean_scores.append(i)
-        inds_clean_scores = torch.LongTensor(inds_clean_scores).to(self.device)
-        clean_boxes = torch.index_select(boxes, 0, inds_clean_scores)
-        clean_labels = torch.index_select(labels, 0, inds_clean_scores).tolist()
-        clean_scores = torch.index_select(scores, 0, inds_clean_scores)
-        image_tensor = torchvision.utils.draw_bounding_boxes(image_tensor.detach().cpu(),
-                                                clean_boxes,
-                                                [self.labels_dict[id] + ": {}%".format(str(round(float(scores[i])*100, 2))) for i, id in enumerate(clean_labels)],
-                                                [self.colors_dict[id] for id in clean_labels],
-                                                fill=False,
-                                                width=2,
-                                                font_size=15)
-        image_tensor = torch.transpose(image_tensor, 2,1)
-        image_tensor = torch.transpose(image_tensor, 2,0)
-        return clean_boxes, clean_labels, image_tensor.numpy()
--- src/pytorch_detection.py
+++ src/pytorch_detection.py
@@ -2,8 +2,8 @@
 
 import rospy
 from sensor_msgs.msg import Image
-from std_msgs.msg import Int32
 from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Bool
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
@@ -15,54 +15,61 @@ class pytorch_detector:
 
     def __init__(self):
 
+        self.bridge = CvBridge()
+        
+        self.model = FaceRCNN()
+
         self.img_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, self.img_cb)
 
         self.img_pub = rospy.Publisher('/person_img', Image, queue_size=1)
 
-        self.bridge = CvBridge()
+        self.person_pub = rospy.Publisher('/person_centroid', Int32MultiArray, queue_size=1)
 
-        self.model = FaceRCNN()
+        self.face_pub = rospy.Publisher('/face_detected', Bool, queue_size=1)
 
 
-    
+
     def img_cb(self, img_msg):
 
         img = self.bridge.imgmsg_to_cv2(img_msg, 'bgr8')
         self.detect(img)
 
-    def detect_and_draw(self):
+    def detect_essential(self, img):
+        boxes, labels, scores = self.model.simple_detection(img)
+        max_score = 0
+        cX = None
+        cY = None
+        face_detected = False
+        for i, box in enumerate(boxes):
+            if scores[i] > max_score and labels[i] == 1:
+                cX = int(box[0] + ((box[2]-box[0])/2))
+                cY = int(box[1] + ((box[3]-box[1])/2))
+                max_score = scores[i]
+            elif labels[i] == 2:
+                face_detected = True
+        person_msg = Int32MultiArray(data=[cX, cY])
+        face_msg = Bool(data=face_detected)
+        self.person_pub.publish(person_msg)
+        self.face_pub.publish(face_msg)
+        
 
-        try:
-            boxes, labels, boxed_img = self.model.person_boxes(self.img)
-            print(boxes)
-            print(labels)
-            boxed_img_msg = self.bridge.cv2_to_imgmsg(boxed_img, 'bgr8')
-
-            self.img_pub.publish(boxed_img_msg)
-        except:
-            pass
 
     def detect(self, img):
 
         try:
-            first_img = img
-            boxes, labels, scores = self.model.simple_detection(first_img)
+            boxes, labels, scores = self.model.simple_detection(img)
             for i, box in enumerate(boxes):
                 if scores[i] > 0.4:
                     if labels[i] == 1:
-                        cv2.rectangle(first_img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,0,0), 4)
+                        cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,0,0), 4)
                     else:
-                        cv2.rectangle(first_img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0,0,255), 4)
-            imgmsg = self.bridge.cv2_to_imgmsg(first_img, 'bgr8')
+                        cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0,0,255), 4)
+            imgmsg = self.bridge.cv2_to_imgmsg(img, 'bgr8')
             self.img_pub.publish(imgmsg)
             print("Published image")
         except Exception as e:
             print(e)
 
-
-# def resize(img):
-#     w = img.shape[0]
-#     h = img.shape[1]
 
 
 if __name__ == '__main__':
