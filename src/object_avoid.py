277859aa73dd11136676118dbe95236f06f13e93
NathanCai2000 <nathancai@brandeis.edu>
2021-05-01 22:20:08 +0000

    Final Push

--- src/object_avoid.py
+++ src/object_avoid.py
@@ -1,45 +1,30 @@
 #!/usr/bin/env python
 
-import rospy, cv2, cv_bridge, numpy
+import rospy, numpy
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 import numpy as np
 import random
-
-"""
-NOTES:
-1) Obstacle avoidance is implemented, but will need to have "refind person" mode implemented
-2) Need to integrate person and face detection code into program
-"""
-
+from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Bool
 
 class Follower:
     def __init__(self):
-        self.bridge = cv_bridge.CvBridge()
-
-        #Change this subscriber to whatever is needed for person rec
-        self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
-
+        self.person = rospy.Subscriber('/person_centroid', Int32MultiArray, self.centroid_cd)
+        self.face_bool = rospy.Subscriber('/face_detected', Bool, self.face_cd)
         self.scan_sub = rospy.Subscriber('scan', LaserScan, self.scan_cb)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
         self.ranges = None
         self.twist = Twist()
-        self.logcount = 0
         self.control = 0
+        self.centroid_data = np.array([-1, -1])
         self.face = False
-
-        #Temperoray Face array for testing
-        #Change to TRUE if you want to test stop
-        self.face_test = [False]
-
-        self.last_image = None
-        self.last_side = False
 
     def scan_cb(self, msg, cone=15):
         if self.face:
             self.go(0, speedlin=0)
-            print("HUMAN!!!!")
+            print("HUMAN SEES ME!!!!")
         else:
             self.ranges =  np.array(msg.ranges)
         
@@ -58,72 +43,26 @@ class Follower:
             elif np.amin(front) < 2:
                 temp_speed += -float(direction) * 2
 
-            print(temp_speed)
+            print("Object avoidance addition speed: ", temp_speed)
             self.go(self.control + temp_speed)
 
-    #Placeholder for person and face detect code
-    #
-    #
-    #
-    #----------vvvvv--------
-    def image_callback(self, msg):
-
-        # get image from camera
-        image = self.bridge.imgmsg_to_cv2(msg)
-
-        # filter out everything that's not yellow
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        lower_yellow = numpy.array([ 40, 0, 0])
-        upper_yellow = numpy.array([ 120, 255, 255])
-        mask = cv2.inRange(hsv,  lower_yellow, upper_yellow)
-        masked = cv2.bitwise_and(image, image, mask=mask)
-
-    # clear all but a 20 pixel band near the top of the image
-        h, w, d = image.shape
-        search_top = 3 * h /4
-        search_bot = search_top + 20
-        mask[0:search_top, 0:w] = 0
-        mask[search_bot:h, 0:w] = 0
-        cv2.imshow("band", mask)
-
-    # Compute the "centroid" and display a red circle to denote it
-        M = cv2.moments(mask)
-        self.logcount += 1
-        print("M00 %d %d" % (M['m00'], self.logcount))
+    def centroid_cd(self, centroids):
+        self.centroid_data = np.array(centroids.data)
+        resolution = [1080, 1920, 0]
+        self.centroid_processor(self.centroid_data, resolution)
     
-        temp = []
-
-        if M['m00'] > 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-
-            temp.append([cx, cy])
-            
-        self.centroid_processor(temp, image.shape, self.face_test)
-        cv2.imshow("image", image)
-        cv2.waitKey(3)
-        print(self.control)
-    #---------^^^^^^^^-----------
-    #
-    #
-    #----------------------------
-
-
+    def face_cd(self, Bool):
+        self.face = Bool.data
     
     #Processes all incoming image data, so to be used later on
-    def centroid_processor(self, centroids, resolution, face_there):
-        self.face = np.any(face_there)
+    def centroid_processor(self, centroids, resolution):
         h, w, d = resolution
         err = 0
-        if len(centroids) != 0:
-            target = 0
-            for x in range(0, len(centroids)):
-                if centroids[x][1] < centroids[target][1]:
-                    target = x
-            err = centroids[target][0] - w/2
+        if not np.array_equal(centroids, [-1, -1]):
+            print("Person location: ", centroids)
+            err = centroids[0] - w/2
             err = -float(err) / 1000
-            self.last_side = centroids[target][0] >= w/2
+            print("Current turn speed: ", err)
         else:
             err = min(max(random.uniform(-1, 1), -1), 1)
 
